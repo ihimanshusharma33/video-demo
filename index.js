d0a7f77e480fe46815de3ec1a7b46dc415e14f7d
Himanshu sharma <hs991009@gmail.com>
2024-11-20 21:31:57 +0530

    route add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("url hit sucessfully");
 })
 
